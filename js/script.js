005b80424b39a92849b688ed5c3c6dabf160b709
Christopher J Lee <christopherlee191.cl@gmail.com>
2019-01-15 23:26:38 +0000

    fff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ['Blue Bird','Again','K/DA','' ];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var artists = ['Ikimono-gakari','Luffy',': Riot Music Team and Harloe']
+var links = ['https://www.youtube.com/watch?v=Dp8EuP_0gWI','https://www.youtube.com/watch?v=2DYYVp4QXew','https://www.youtube.com/watch?v=UOxkGD8qRB4']
+var picture=['']
+var times=['3:38','4:11','3:22']
+
+ songs.forEach(function(song){
+     $("#songs").append("<li>" + song + "</li>");
+     
+ 
+     
+ })
+ artists.forEach(function(artist){
+    $("#artists").append("<p>" + artist + "</p>");
+ })
+times.forEach(function(time){
+    $("#lengths").append("<p>" + time + "</p>");
+})
+links.forEach(function(link){
+    $("#links").append('<a href = ' + link + '> </a>');
+})
+
+
 
 
 
